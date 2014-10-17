0f32247426aeefdfdbe09fe815976248ac7739c7
Ganesh Thiagarajan <ganesh_vjy@yahoo.com>
2014-10-16 20:13:16 -0400

    Uploaded

---
+++ TowerOfHanoi.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TOH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("usage: hanoi <number>");
+                System.Environment.Exit(1);
+            }
+
+            int num = System.Convert.ToInt32(args[0]);
+
+            towers(num, 'A', 'C', 'B');
+            System.Environment.Exit(0);
+
+        }
+        public static void towers(int num,char from,char to,char aux){
+            if (num <= 1)
+            {
+                Console.WriteLine("Move disk 1 from ");
+                Console.Write(from);
+                Console.Write("to ");
+                Console.Write(to);
+                return;
+            }
+                
+            towers(num - 1, from, aux, to);
+            Console.WriteLine("Move disk from ");
+            Console.Write(from);
+            Console.Write("to ");
+            Console.Write(to);
+            towers(num - 1, aux, to, from);
+        }
+    }
+}
